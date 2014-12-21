b48206e0e59f298fd39b6b4e92cf602402c29493
Thomas Wenzlaff <pi-entw@wenzlaff.de>
2014-12-21 16:08:08 +0000

    Neues tree Script ergänzt

---
+++ make-tree.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# make-tree.sh
+#
+# Erstellt einen abhängigkeits Graphen.
+#
+# Thomas Wenzlaff 21.12.2014 Installationsanleitung unter http://www.wenzlaff.info
+#
+# (C) 2014 Thomas Wenzlaff http://www.wenzlaff.de
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see {http://www.gnu.org/licenses/}.
+#
+# Aufruf: ./make-tree Package
+
+
+mkdir $1
+cd $1
+debtree $1 > $1.dot
+dot -Tpng -o $1.png $1.dot
